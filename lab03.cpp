3a9879385e9c3f3fba7dc7570eb91b25fc485435
SavvinYR <Yaroslav.savvin@mail.ru>
2022-06-05 23:14:17 +0300

    "Build:cuel_end"

--- lab03.cpp
+++ lab03.cpp
@@ -47,12 +47,18 @@ Input read_input(istream& in, bool prompt) {
 
 int main(int argc, char* argv[])
 { if (argc > 1){
-cout<<endl;
-for(int i=0; i<argc; i++)
-    cout<<endl<< argv[i];
-    cout<<endl;
+    CURL *curl = curl_easy_init();
+if(curl) {
+  CURLcode res;
+  curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+  res = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
+  if (!(res == 0)){
+    cout<< curl_easy_strerror(res);
+    exit(1);}
+}
     return 0;}
-    curl_global_init(CURL_GLOBAL_ALL);
+curl_global_init(CURL_GLOBAL_ALL);
 
 
 
