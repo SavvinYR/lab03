69637a2fdfc462d6daf5ff44e6f1aba854c8674a
SavvinYR <Yaroslav.savvin@mail.ru>
2022-06-05 17:57:23 +0300

    Revert "'Lab03'"
    This reverts commit 8754e55c9e8572371654181341b320e6d05d1c66.
    
    'Ступили'

--- histogram.h
+++ histogram.h
@@ -7,7 +7,6 @@
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max);
-vector<size_t> make_histogram (vector<double>numbers, size_t bin_count, double min, double max);
-void show_histogram_text(vector<size_t> bins);
+
 
 #endif // HISTOGRAM_H_INCLUDED
