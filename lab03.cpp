e130dafa3f1e0dfd0a318fad1a790c8ef08453ce
SavvinYR <Yaroslav.savvin@mail.ru>
2022-06-05 18:07:37 +0300

    Lab04

--- lab03.cpp
+++ lab03.cpp
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string>
 #include "histogram.h"
+#include "svg.cpp"
 using namespace std;
 
 vector<double> input_numbers(size_t count) //функция ввода чисел
@@ -80,72 +81,7 @@ void show_histogram_text(vector<size_t> bins)
 
 }
 
-void
-svg_begin(double width, double height)
-{
-    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
-    cout << "<svg ";
-    cout << "width='" << width << "' ";
-    cout << "height='" << height << "' ";
-    cout << "viewBox='0 0 " << width << " " << height << "' ";
-    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
-}
 
-void
-svg_end()
-{
-    cout << "</svg>\n";
-}
-
-void svg_text(double left, double baseline, string text)
-{
-    cout << "<text x='" << left << "' y='"<< baseline << "'>'"<< text <<"'</text>";
-}
-
-void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black"){
-    cout << "<rect x='"<< x <<"' y='"<< y <<"' width='"<< width <<"' height='"<< height <<"' stroke='"<< stroke <<"' fill='"<< fill <<"'></rect>\n";
-
-}
-
-
-
-
-
-void
-show_histogram_svg(const vector<size_t>& bins)
-{
-    const auto IMAGE_WIDTH = 400;
-const auto IMAGE_HEIGHT = 300;
-const auto TEXT_LEFT = 20;
-const auto TEXT_BASELINE = 20;
-const auto TEXT_WIDTH = 50;
-const auto BIN_HEIGHT = 30;
-const auto BLOCK_WIDTH = 10;
-    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
-    double top = 0;
-    double MAX = (IMAGE_WIDTH - TEXT_WIDTH)/BLOCK_WIDTH;//Масштабирование
-    size_t max_bin = bins[0];
-    for(size_t bin : bins)
-    {
-        if(bin > max_bin)
-        {
-            max_bin = bin;
-        }
-    }
-    for (size_t bin : bins)
-    {double height = bin;
-        if(max_bin > MAX)   //Если масштабирование необходимо
-        {
-             height = MAX * (static_cast<double>(bin)/max_bin);
-        }
-        const double bin_width = BLOCK_WIDTH * bin;
-
-        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
-        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "#CD5C5C", "#CD5C5C");
-        top += BIN_HEIGHT;
-    }
-    svg_end();
-}
 
 
 
