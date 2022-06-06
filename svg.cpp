3c68f4edc4c9ac89f288a42580c3da72ffe122c6
SavvinYR <Yaroslav.savvin@mail.ru>
2022-06-06 17:28:50 +0300

    End

--- svg.cpp
+++ svg.cpp
@@ -28,6 +28,36 @@ void svg_rect(double x, double y, double width, double height, string stroke, st
 
 }
 
+string make_info_text() {
+    stringstream buffer;
+    const auto R =GetVersion();
+   printf("n = %lu\n", R);
+printf("n = %lx\n", R);
+DWORD mask = 0b00000000'00000000'11111111'11111111;
+DWORD version =R&mask;
+printf("ver = %lu\n",version);
+DWORD platform = R >> 16;
+printf("ver2 = %lu\n",platform);
+DWORD mask2=0b00000000'11111111;
+DWORD version_major=version&mask2;
+printf("version_major = %lu\n",version_major);
+DWORD version_minor=version>>8;
+printf("version_minor = %lu\n",version_minor);
+DWORD build;
+if ((R & 0x80000000) == 0)
+{
+   build =platform;
+   printf("build = %lu\n",build);
+
+}
+buffer<<"Windows"<< " " <<"v"<< " " <<version_major<<"."<<version_minor<< " "<< "(build"<< " "<<build<<")"<<endl;
+TCHAR storage [ MAX_COMPUTERNAME_LENGTH + 1];
+DWORD  bufCharCount = MAX_COMPUTERNAME_LENGTH+1;
+GetComputerNameA(storage,&bufCharCount);
+buffer<<"Computer name:"<<" "<<storage;
+    return buffer.str();
+}
+
 void show_histogram_svg(const vector<size_t>& bins)
 {
     const auto IMAGE_WIDTH = 400;
@@ -49,6 +79,7 @@ void show_histogram_svg(const vector<size_t>& bins)
         top += BIN_HEIGHT;
 
     }
+    svg_text(TEXT_LEFT, top += BIN_HEIGHT, make_info_text());
     svg_end();
 
 }
